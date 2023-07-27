2317819ec35c6af07bfac97e303ef1a93bbbf303
kcritesh <kcritesh2059@gmail.com>
2023-07-28 02:28:57 +0545

    verifyemaildone

--- src/views/verifyEmail.tsx
+++ src/views/verifyEmail.tsx
@@ -1,13 +1,34 @@
+import React from "react";
 import { useLocation, useNavigate, useParams } from "react-router-dom";
+import axiosConfig from "src/config/axios.config";
 
 const verifyEmail = () => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   const location = useLocation();
   const token = new URLSearchParams(location.search).get("token");
+  const [verifyingEmail, setVerifyingEmail] = React.useState(false);
+  const [error, setError] = React.useState(false);
+  const [success, setSuccess] = React.useState(false);
+  React.useEffect(() => {
+    setVerifyingEmail(true);
+    if (token) {
+      axiosConfig
+        .post(`/api/auth/verify-email/${token}`)
+        .then((res) => {
+          setSuccess(true);
+          navigate("/login");
+        })
+        .catch((err) => {
+          setError(true);
+          console.log(err.response.data);
+        });
+    }
+  }, [token]);
   return (
     <div>
-      <h1>Verify Email</h1>
-      <p>Token: {token}</p>
+      {verifyingEmail && <h1>Verifying Email...</h1>}
+      {error && <h1>Error</h1>}
+      {success && <h1>Success</h1>}
     </div>
   );
 };
